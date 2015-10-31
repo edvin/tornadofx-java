ac33699bed755cf16f692d80877bbf789a8f4bf2
edvin <es@syse.no>
2015-10-31 16:27:08 +0100

    Added JsonModel copy to automatically copy all properties to another model object

--- src/main/java/tornadofx/JsonModel.java
+++ src/main/java/tornadofx/JsonModel.java
@@ -5,9 +5,34 @@ import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 
 public interface JsonModel {
+	/**
+	 * Fetch JSON values and update the model properties
+	 * @param json The json to extract values from
+	 */
 	void updateModel(JsonObject json);
+
+	/**
+	 * Build a JSON representation of the model properties
+	 * @param builder A builder that should be filled with the model properties
+	 */
 	void toJSON(JsonObjectBuilder builder);
 
+	/**
+	 * Copy all properties from this object to the given target object by converting to JSON and then updating the target.
+	 * @param target The target object to update with the properties of this model
+	 */
+	default void copy(JsonModel target) {
+		JsonObjectBuilder builder = Json.createObjectBuilder();
+		toJSON(builder);
+		target.updateModel(builder.build());
+	}
+
+	/**
+	 * Clone this model object by creating a new object of the same type and copy over all the model properties.
+	 *
+	 * @param <T> The type of object
+	 * @return A new object of type T with the model properties of this object
+	 */
 	default <T extends JsonModel> T clone() {
 		try {
 			T clone = (T) getClass().newInstance();
