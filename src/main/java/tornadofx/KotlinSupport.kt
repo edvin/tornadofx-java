116b4565e2cba0f4615abc14a8c07e9032b8397c
edvin <es@syse.no>
2015-11-21 22:31:48 +0200

    Kotlinified Kotlin support

--- src/main/java/tornadofx/KotlinSupport.kt
+++ src/main/java/tornadofx/KotlinSupport.kt
@@ -2,7 +2,6 @@ package tornadofx
 
 import javafx.collections.ObservableList
 import javafx.scene.Node
-import kotlin.reflect.KClass
 
 inline fun <reified T : Fragment<out Node>> fragment(): T =
         InjectionContext.get(T::class.java)
@@ -13,8 +12,8 @@ inline fun <reified T : Controller> controller(): T =
 inline fun <reified T : View<out Node>> view(): T =
         InjectionContext.get(T::class.java)
 
-fun <Model : JsonModel> Rest.JsonObjectResult.to(objectType: KClass<Model>) : Model =
-        to(objectType.java)
+inline fun <reified Model : JsonModel> Rest.JsonObjectResult.to() : Model =
+        to(Model::class.java)
 
-fun <Model : JsonModel> Rest.JsonArrayResult.to(objectType: KClass<Model>) : ObservableList<Model> =
-        to(objectType.java)
+inline fun <reified Model : JsonModel> Rest.JsonArrayResult.to() : ObservableList<Model> =
+        to(Model::class.java)
