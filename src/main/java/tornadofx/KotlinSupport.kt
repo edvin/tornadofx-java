0c655db176e203db9b0541626f67e97f1163959c
edvin <es@syse.no>
2015-12-10 22:28:27 +0100

    Experimental injection of Kotlin val. Still need to be processed in InjectionContext

--- src/main/java/tornadofx/KotlinSupport.kt
+++ src/main/java/tornadofx/KotlinSupport.kt
@@ -2,6 +2,8 @@ package tornadofx
 
 import javafx.collections.ObservableList
 import javafx.scene.Node
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 inline fun <reified T : Fragment<out Node>> fragment(): T =
         InjectionContext.get(T::class.java)
@@ -17,3 +19,10 @@ inline fun <reified Model : JsonModel> Rest.JsonObjectResult.toModel() : Model =
 
 inline fun <reified Model : JsonModel> Rest.JsonArrayResult.toModel() : ObservableList<Model> =
         toModel(Model::class.java)
+
+@Suppress("UNCHECKED_CAST")
+inline public fun <reified T : Component> inject(): ReadOnlyProperty<Component, T> = object : ReadOnlyProperty<Component, T> {
+    override fun getValue(thisRef: Component, property: KProperty<*>): T {
+        return InjectionContext.get(T::class.java)
+    }
+}
