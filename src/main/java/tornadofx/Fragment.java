aff85dd37105f328641559ac16d4bceb035ef987
Edvin Syse <es@syse.no>
2015-12-22 09:38:59 +0100

    Added StageStyle parameter to openModal

--- src/main/java/tornadofx/Fragment.java
+++ src/main/java/tornadofx/Fragment.java
@@ -8,19 +8,24 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import lombok.Getter;
 
 public abstract class Fragment<NodeType extends Node> extends UIComponent<NodeType> implements EventCapable {
 	@Getter private Stage modalStage;
 
-	public void openModal() {
+    public void openModal() {
+        openModal(StageStyle.DECORATED);
+    }
+
+	public void openModal(StageStyle stageStyle) {
 		if (modalStage != null)
 			return;
 
 		if (!(getNode() instanceof Parent))
 			throw new IllegalArgumentException("Only Parent Fragments can be opened in a Modal");
 
-		modalStage = new Stage();
+		modalStage = new Stage(stageStyle);
 		modalStage.titleProperty().bind(titleProperty());
 		modalStage.initModality(Modality.APPLICATION_MODAL);
 
