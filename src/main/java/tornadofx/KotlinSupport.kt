3228807b3ffc1a942f12f0d57b5283ddff6d0fb1
Edvin Syse <es@syse.no>
2016-01-01 21:15:41 +0100

    Nicer async interface for Kotlin

--- src/main/java/tornadofx/KotlinSupport.kt
+++ src/main/java/tornadofx/KotlinSupport.kt
@@ -2,6 +2,7 @@ package tornadofx
 
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
+import javafx.concurrent.Task
 import javafx.scene.Node
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
@@ -22,6 +23,12 @@ inline fun <reified Model : JsonModel> Rest.JsonArrayResult.toModel() : Observab
         toModel(Model::class.java)
 
 fun <T> List<T>.observable() = FXCollections.observableList(this)
+
+fun <T> Component.background(func: () -> T): Task<T> = async(func)
+
+infix fun <T> Task<T>.ui(func: (T) -> Unit) {
+    setOnSucceeded { func(value) }
+}
 
 @Suppress("UNCHECKED_CAST")
 inline public fun <reified T : Component> inject(): ReadOnlyProperty<Component, T> = object : ReadOnlyProperty<Component, T> {
